daf1b2eea726e01f542406659a5cb40c6a6ad219
DESKTOP-Radhe\radhe <radhey.gor@gmail.com>
2021-01-14 00:32:39 -0500

    added chip group for news type

--- app/src/main/java/com/radhecodes/cbctest/MainActivity.kt
+++ app/src/main/java/com/radhecodes/cbctest/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.radhecodes.cbctest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -14,8 +18,10 @@ import com.radhecodes.cbctest.repository.model.News
 import com.radhecodes.cbctest.repository.model.StatusType
 import com.radhecodes.cbctest.ui.NewsViewModel
 import com.radhecodes.cbctest.utils.TopSpacingItemDecoration
-import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
     private val newsViewModel: NewsViewModel by viewModel()
@@ -27,40 +33,65 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.swipeToRefresh.setOnRefreshListener(this)
-        binding.swipeToRefresh.setColorSchemeResources(R.color.purple_200, R.color.purple_200, R.color.purple_200 )
+        binding.swipeToRefresh.setColorSchemeResources(
+            R.color.holo_purple,
+            R.color.holo_purple,
+            R.color.holo_purple
+        )
         initList()
         fetchNews()
         newsViewModel.getNewsFromLocalDb().observe(this, Observer {
             if (it.isNotEmpty()) {
                 recyclerAdapter.submitList(it)
                 val types = getTypes(it)
-                for (type in types) {
-                    val chip = Chip(this)
-                    chip.text = type
-                    types_chip.addView(chip)
+                if (types.isNotEmpty()) {
+                    binding.typesChip.removeAllViews()
+                    for (type in types) {
+                        val chip = Chip(this)
+                        chip.id = ViewCompat.generateViewId()
+                        chip.isCheckable = true
+                        chip.isClickable = true
+                        chip.text = type.toUpperCase(Locale.ROOT)
+                        binding.typesChip.addView(chip)
+                    }
+                    binding.typesChip.setOnCheckedChangeListener { group, checkedId ->
+                        val chip: Chip? = group.findViewById(checkedId)
+                        chip?.let {
+                            Toast.makeText(this, chip.text, Toast.LENGTH_SHORT).show()
+                        } ?: kotlin.run {
+                        }
+                    }
                 }
             } else {
-            //TODO() no data template
+                //TODO() no data template
             }
         })
     }
 
     private fun fetchNews() {
-        newsViewModel.getNewsFromApi().observe(this, Observer { status ->
-            if(status?.status == StatusType.SUCCESS) {
+        newsViewModel.getNewsFromApi().observe(this, { status ->
+            if (status?.status == StatusType.SUCCESS) {
                 binding.noNetworkTemplate.visibility = View.GONE
                 binding.chipsScroll.visibility = View.VISIBLE
                 binding.newsList.visibility = View.VISIBLE
                 val list = status.isInstanceOf<List<ApiResponseItem>>()
                 if (list != null) {
-                    newsList = list.map { News(it.getNewsId()!!, it.getTitle()!!, it.getImageUrl()!!, it.getPublishedTime()!!, it.getType()!!) }
+                    newsList = list.map {
+                        News(
+                            it.getNewsId()!!,
+                            it.getTitle()!!,
+                            it.getImageUrl()!!,
+                            it.getPublishedTime()!!,
+                            it.getType()!!
+                        )
+                    }
                 }
                 newsViewModel.deleteAndInsertAllNews(newsList)
                 if (binding.swipeToRefresh.isRefreshing) {
                     binding.swipeToRefresh.isRefreshing = false
                 }
             } else {
-                    //TODO() error toast
+                //TODO() error toast
                 if (binding.swipeToRefresh.isRefreshing) {
                     binding.swipeToRefresh.isRefreshing = false
                 }
@@ -82,8 +113,8 @@ class MainActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         return newsList.map{ it.getType() }.toSet().toList()
     }
 
-    private fun getNewsFromType(type: String, newsList: List<ApiResponseItem>?): List<ApiResponseItem?>? {
-        return newsList?.filter { it.getType() == type }
+    private fun getNewsFromType(type: String, newsList: List<News>): List<News>{
+        return newsList.filter { it.getType() == type }
     }
 
     override fun onRefresh() {
